eb148dbb88f2f23914fc352f90efabf4377e50d6
kayquelopes <kayquelopes353@gmail.com>
2025-04-01 14:18:35 -0300

    feat: criação da API para Categoria e mudança no codigo

--- core/models/Categoria.py
+++ core/models/Categoria.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Categoria(models.Model):
-    descrição = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
 
-        return self.descrição
+        return self.descricao
