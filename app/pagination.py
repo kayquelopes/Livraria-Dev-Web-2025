6e33442cc53bbb7aa4b7e6f59355357839d9cd0f
kayquelopes <kayquelopes353@gmail.com>
2025-04-01 14:19:28 -0300

    feat: criação do pagination

--- app/pagination.py
+++ app/pagination.py
@@ -6,3 +6,7 @@ class CustomPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         return Response({
             'page': self.page.number,
+            'page_size': self.page.paginator.per_page,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data,
+        })
